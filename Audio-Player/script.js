72720ccff774fe2087b1896f338ed8409117b00c
Deep-Gohil <deep72617626@gmail.com>
2024-07-15 17:00:59 +0530

    Audio-Player

--- Audio-Player/script.js
+++ Audio-Player/script.js
@@ -2,55 +2,55 @@ let musics = [
     {
         image: "https://i1.sndcdn.com/artworks-DCgWFLn6HwboMbgy-FggHsw-t1080x1080.jpg",
         name: "Be Mine",
-        audio: 'https://rr3---sn-cvh76nll.googlevideo.com/videoplayback?expire=1721041647&ei=j66UZt_GF5ubi9oPp6CD4AI&ip=23.88.68.133&id=o-AEiukKPfwDGaoRejczZqaqgNWGRmwQYRUWQ_7DQvOZJ2&itag=251&source=youtube&requiressl=yes&xpc=EgVo2aDSNQ%3D%3D&vprv=1&svpuc=1&mime=audio%2Fwebm&rqh=1&gir=yes&clen=6137187&dur=342.521&lmt=1720869846013179&keepalive=yes&c=ANDROID_TESTSUITE&txp=6308224&sparams=expire%2Cei%2Cip%2Cid%2Citag%2Csource%2Crequiressl%2Cxpc%2Cvprv%2Csvpuc%2Cmime%2Crqh%2Cgir%2Cclen%2Cdur%2Clmt&sig=AJfQdSswRQIhAJfvkZdP99hm2jw4hMcfEzxd_TQDzhspR-OEQtv9JJ0zAiAQPT3AnyfZTlNdMdBflHm3o3CBKEvcEfszSHgqpb5IDQ%3D%3D&rm=sn-4g5edl7l&fexp=24350516,24350518&req_id=c692e7719ab8a3ee&ipbypass=yes&redirect_counter=2&cm2rm=sn-bu2a-nu867e&cms_redirect=yes&cmsv=e&mh=vm&mip=27.116.48.74&mm=29&mn=sn-cvh76nll&ms=rdu&mt=1721019898&mv=m&mvi=3&pl=24&lsparams=ipbypass,mh,mip,mm,mn,ms,mv,mvi,pl&lsig=AHlkHjAwRgIhANvgHzG8hWZhlLu_ENgg7BLukmMGN5LuTHQQBim2KEeLAiEA9ueRYssHC2iDNbqjqdmYQxayJjX6PZLXdcawuZwaRkU%3D',
+        audio: './Audio/Be Mine - Shubh.mp3',
         icon: `<i class="fa-solid fa-play"></i>`
     },
     {
         image: "https://www.musiculture.in/wp-content/uploads/2024/06/PHOTO-2024-06-25-14-01-19-1-800x800.jpg",
         name: "Chahun",
-        audio: 'https://rr1---sn-bu2a-nu86.googlevideo.com/videoplayback?expire=1721035244&ei=jJWUZqzCFJmw6dsP5YiawAE&ip=162.55.128.6&id=o-ABBMVs6URMiAkb37xdPHgoebSC7FTBpSSQlW4lUMaV0T&itag=251&source=youtube&requiressl=yes&xpc=EgVo2aDSNQ%3D%3D&vprv=1&svpuc=1&mime=audio%2Fwebm&rqh=1&gir=yes&clen=3518915&dur=200.621&lmt=1719848792835405&keepalive=yes&c=ANDROID_TESTSUITE&txp=4532434&sparams=expire%2Cei%2Cip%2Cid%2Citag%2Csource%2Crequiressl%2Cxpc%2Cvprv%2Csvpuc%2Cmime%2Crqh%2Cgir%2Cclen%2Cdur%2Clmt&sig=AJfQdSswRQIgQ0uwzLZnD5SZUw2TsRhHbtxcvGTKtk74jYj84mBFB-YCIQCSZAXQ2T8iLxxDCOiaJkmiRWi3AFyDN4w2dZurLedJ-Q%3D%3D&redirect_counter=1&rm=sn-4g5edl7e&fexp=24350516,24350518&req_id=970795a7fb48a3ee&cms_redirect=yes&cmsv=e&ipbypass=yes&mh=9G&mip=27.116.48.74&mm=31&mn=sn-bu2a-nu86&ms=au&mt=1721020229&mv=u&mvi=1&pl=24&lsparams=ipbypass,mh,mip,mm,mn,ms,mv,mvi,pl&lsig=AHlkHjAwRgIhAK8vCkfcu_2fEjCqPqmIw7f23VFWm0lQ6XW5Kn_ANdV6AiEAn3xt61Wq_4Hfoc7hNIdVtCrdTPzTrGmSDuMxV7l4AcU%3D',
+        audio: './Audio/Chahun - Stebin Ben.mp3',
         icon: `<i class="fa-solid fa-play"></i>`
     },
     {
         image: "https://c.saavncdn.com/057/Gulabi-Gaal-Hindi-2024-20240704040002-500x500.jpg",
         name: "Gulabi Gal",
-        audio: 'https://rr3---sn-bu2a-nu86.googlevideo.com/videoplayback?expire=1721025714&ei=UnCUZvXEMeuUsvQP4tmxyA8&ip=23.88.69.123&id=o-ANxqRLqneFtZRKIi83TPEvCp2EuVSzHQDn3FZslykTL3&itag=251&source=youtube&requiressl=yes&xpc=EgVo2aDSNQ%3D%3D&vprv=1&svpuc=1&mime=audio%2Fwebm&rqh=1&gir=yes&clen=3979453&dur=238.181&lmt=1718333384497031&keepalive=yes&c=ANDROID_TESTSUITE&txp=4532434&sparams=expire%2Cei%2Cip%2Cid%2Citag%2Csource%2Crequiressl%2Cxpc%2Cvprv%2Csvpuc%2Cmime%2Crqh%2Cgir%2Cclen%2Cdur%2Clmt&sig=AJfQdSswRQIgaZmI0X7YrkRc6mjyB54lyZZdqZtVr1zXwfEf8xxbq_4CIQCKF6GRmOgZtj5gSU8vir4KCO4r6PNAr9bNT80bP80zvQ%3D%3D&redirect_counter=1&rm=sn-4g5e6d7z&rrc=104&fexp=24350516,24350518&req_id=40bed9a9e236a3ee&cms_redirect=yes&cmsv=e&ipbypass=yes&mh=iM&mip=27.116.48.74&mm=31&mn=sn-bu2a-nu86&ms=au&mt=1721020229&mv=u&mvi=3&pl=24&lsparams=ipbypass,mh,mip,mm,mn,ms,mv,mvi,pl&lsig=AHlkHjAwRQIhAKx6eJrhtm07fMUk0ZVrABYKuqhH8CbmsmcRaCKFN2l2AiAcCKXCvLgdurrOeaZDyqWlMg96Hatl8GStyp_b9gOamw%3D%3D',
+        audio: './Audio/Gulabi Gaal - Saaj Bhatt.mp3',
         icon: `<i class="fa-solid fa-play"></i>`
     },
     {
         image: "https://c.saavncdn.com/858/Aavesham-Malayalam-2024-20240514204401-500x500.jpg",
         name: "Illuminati",
-        audio: 'https://rr2---sn-bu2a-nu8e.googlevideo.com/videoplayback?expire=1721040146&ei=sqiUZpz9DpDk6dsP_-ylmA0&ip=188.40.52.28&id=o-AJyvDj3B35gdyomlo0231w9t0BUKiNEWhxYCiwVhlWFm&itag=251&source=youtube&requiressl=yes&xpc=EgVo2aDSNQ%3D%3D&vprv=1&svpuc=1&mime=audio%2Fwebm&rqh=1&gir=yes&clen=2442454&dur=148.201&lmt=1715024592685607&keepalive=yes&c=ANDROID_TESTSUITE&txp=4502434&sparams=expire%2Cei%2Cip%2Cid%2Citag%2Csource%2Crequiressl%2Cxpc%2Cvprv%2Csvpuc%2Cmime%2Crqh%2Cgir%2Cclen%2Cdur%2Clmt&sig=AJfQdSswRgIhALoXHY35OjE2AKSMfoekcTHItFV9YPri6NGwD8-6SxTnAiEAp6dcspVm_2-eYpOn8hCcrClw71_PLrral2T1BxiSID4%3D&redirect_counter=1&rm=sn-4g5ekz76&fexp=24350516,24350518&req_id=100e4cef73dba3ee&cms_redirect=yes&cmsv=e&ipbypass=yes&mh=kA&mip=27.116.48.74&mm=31&mn=sn-bu2a-nu8e&ms=au&mt=1721020229&mv=u&mvi=2&pcm2cms=yes&pl=24&lsparams=ipbypass,mh,mip,mm,mn,ms,mv,mvi,pcm2cms,pl&lsig=AHlkHjAwRAIgSObpcJOpksXskrVB5IBgzFOWuVi0eBiUIYs-DVQWljoCIEpvAj30K261LNKj5mTkkfm_PAc3D9QBQzWiCKquRSxj',
+        audio: './Audio/Illuminati.mp3',
         icon: `<i class="fa-solid fa-play"></i>`
     },
     {
         image: "https://c.saavncdn.com/777/Pagal-Punjabi-2019-20190909161954-500x500.jpg",
         name: "Pagal",
-        audio: 'https://rr1---sn-bu2a-nu86.googlevideo.com/videoplayback?expire=1721032558&ei=DouUZrifIdmfi9oPo8ir8Ac&ip=162.55.96.91&id=o-AFqAUpZ3ATbn0n_l8NhtAlS3MtrMo53PsP31HSogmLgt&itag=251&source=youtube&requiressl=yes&xpc=EgVo2aDSNQ%3D%3D&vprv=1&svpuc=1&mime=audio%2Fwebm&rqh=1&gir=yes&clen=3491023&dur=197.061&lmt=1716844802052981&keepalive=yes&c=ANDROID_TESTSUITE&txp=4532434&sparams=expire%2Cei%2Cip%2Cid%2Citag%2Csource%2Crequiressl%2Cxpc%2Cvprv%2Csvpuc%2Cmime%2Crqh%2Cgir%2Cclen%2Cdur%2Clmt&sig=AJfQdSswRQIhALOJGYFjOFrksAeFdSxBcwQ9bHnFmr4PZz20torbFtzeAiADbaFdhgTOHsHl_G4zUnNApeSfXfh5sWaXcagBkRFIfw%3D%3D&redirect_counter=1&rm=sn-4g5ekd7z&fexp=24350516,24350518&req_id=35f42be8e8ba3ee&cms_redirect=yes&cmsv=e&ipbypass=yes&mh=uR&mip=27.116.48.74&mm=31&mn=sn-bu2a-nu86&ms=au&mt=1721020229&mv=u&mvi=1&pcm2cms=yes&pl=24&lsparams=ipbypass,mh,mip,mm,mn,ms,mv,mvi,pcm2cms,pl&lsig=AHlkHjAwRgIhALxKDVkvb5jR2OzbK-9xTLHSNMOEGAKlqS9SXqNGcTb_AiEA1W2B_XHXlvUQVBh_2O9waywBVM7irRl11Tq764gAzAI%3D',
+        audio: './Audio/Pagal - Guru Randhawa.mp3',
         icon: `<i class="fa-solid fa-play"></i>`
     },
     {
         image: "https://c.saavncdn.com/511/Paon-Ki-Jutti-Punjabi-2024-20240504053321-500x500.jpg",
         name: "Paun Ki jooti",
-        audio: 'https://rr5---sn-gwpa-5bge.googlevideo.com/videoplayback?expire=1721042752&ei=4LKUZtmtDqW09fwPkuaRoAY&ip=2409%3A40d2%3A50%3Afb01%3A21b9%3A5bb1%3A159a%3A7ecb&id=o-AC5ssuS30on46tNK452fvUyyfsi1m-i3RW0g5M4wfFIz&itag=251&source=youtube&requiressl=yes&xpc=EgVo2aDSNQ%3D%3D&mh=Wi&mm=31%2C29&mn=sn-gwpa-5bge%2Csn-gwpa-qxak&ms=au%2Crdu&mv=m&mvi=5&pl=36&gcr=in&initcwndbps=827500&bui=AXc671K6E2-X06CGNlkqP6btTMz-2td7RDsjUjG0A0tnlUf68km7XptyiD59qsupvDdi5feETdba8fI2&vprv=1&svpuc=1&mime=audio%2Fwebm&ns=H1wBGjV-4ICapUhpumueu64Q&rqh=1&gir=yes&clen=5337028&dur=322.301&lmt=1720924349669498&mt=1721020856&fvip=6&keepalive=yes&c=TVHTML5_SIMPLY_EMBEDDED_PLAYER&sefc=1&txp=5532434&n=i5hX3VAMBy7SAg&sparams=expire%2Cei%2Cip%2Cid%2Citag%2Csource%2Crequiressl%2Cxpc%2Cgcr%2Cbui%2Cvprv%2Csvpuc%2Cmime%2Cns%2Crqh%2Cgir%2Cclen%2Cdur%2Clmt&lsparams=mh%2Cmm%2Cmn%2Cms%2Cmv%2Cmvi%2Cpl%2Cinitcwndbps&lsig=AHlkHjAwRAIgWqIKx6RFnOXkf2G1cnyCdbwifMEMoe2S6bIztsOg3VMCIF4Ik2OozjV20va17RfkHPhrCL75wMPO7prWl4zeAzbe&sig=AJfQdSswRgIhAMcwjV3gDa8wzet1BZNNKt2IZr_aaRxielfk4Jauhn65AiEAwcZYj7ENgyXdB93Mm8EGZm4SkG6RlnbWXOt-9YJZ0zA%3D',
+        audio: './Audio/Paon Ki Jutti.mp3',
         icon: `<i class="fa-solid fa-play"></i>`
     },
     {
         image: "https://c.saavncdn.com/567/Papa-Kehte-Hain-From-Srikanth-Hindi-2024-20240419191003-500x500.jpg",
         name: "Papa Kehte Hai",
-        audio: 'https://rr3---sn-bu2a-nu8s.googlevideo.com/videoplayback?expire=1721042787&ei=A7OUZpHvKKe56dsPst65YA&ip=162.55.128.6&id=o-AOkyij5vvkPfC-ETs158kdIx1D7gNNMFaARcmC3-kNU9&itag=251&source=youtube&requiressl=yes&xpc=EgVo2aDSNQ%3D%3D&vprv=1&svpuc=1&mime=audio%2Fwebm&rqh=1&gir=yes&clen=2983283&dur=174.361&lmt=1714709077544805&keepalive=yes&c=ANDROID_TESTSUITE&txp=4532434&sparams=expire%2Cei%2Cip%2Cid%2Citag%2Csource%2Crequiressl%2Cxpc%2Cvprv%2Csvpuc%2Cmime%2Crqh%2Cgir%2Cclen%2Cdur%2Clmt&sig=AJfQdSswRQIhAMd8ucMSMMn4dnihT9PCICBNG40JB1Ze70py68DGQPbTAiAqZj3RR2hWMev_BhO1xfjqAdDSGTNE_TFUtwezOteuew%3D%3D&redirect_counter=1&rm=sn-4g5ere76&fexp=24350516,24350518&req_id=87c1e93a8ac0a3ee&cms_redirect=yes&cmsv=e&ipbypass=yes&mh=u4&mip=27.116.48.74&mm=31&mn=sn-bu2a-nu8s&ms=au&mt=1721020229&mv=u&mvi=3&pcm2cms=yes&pl=24&lsparams=ipbypass,mh,mip,mm,mn,ms,mv,mvi,pcm2cms,pl&lsig=AHlkHjAwRQIgR_3ekoKG4xN9eLBOHtkNB19zut04eTF0DpAB1Y8sjwUCIQC9BKJL-GBI0kDBMOVOiFqP3NuR36_w0XQZxAKGfH3iUg%3D%3D',
+        audio: './Audio/Papa Kehte Hain - Srikanth.mp3',
         icon: `<i class="fa-solid fa-play"></i>`
     },
     {
         image: "https://www.thestatesman.com/wp-content/uploads/2024/05/Allu-Arjun-in-Pushpa-Pushpa-song.jpg",
         name: "Pushpa Pushpa",
-        audio: 'https://rr1---sn-bu2a-nu86.googlevideo.com/videoplayback?expire=1721026753&ei=YXSUZqvkLrXWi9oP_MOq6A8&ip=23.88.69.123&id=o-AH2dYIF7MN6Po3IjIdOfirjA33Ddy2qrwncQIhpf0KBH&itag=251&source=youtube&requiressl=yes&xpc=EgVo2aDSNQ%3D%3D&vprv=1&svpuc=1&mime=audio%2Fwebm&rqh=1&gir=yes&clen=4386393&dur=259.741&lmt=1716116313359479&keepalive=yes&c=ANDROID_TESTSUITE&txp=5532434&sparams=expire%2Cei%2Cip%2Cid%2Citag%2Csource%2Crequiressl%2Cxpc%2Cvprv%2Csvpuc%2Cmime%2Crqh%2Cgir%2Cclen%2Cdur%2Clmt&sig=AJfQdSswRQIgQ3SePnDYwPg0PcTSbhDTDLUAuHkMbEJ6NgOyyRvVrkkCIQD6qWQQs-Q1YeliOys5hnGwqB5_Ux-ZEN8BfDgRyGcKWw%3D%3D&redirect_counter=1&rm=sn-4g5ezz7s&fexp=24350516,24350518&req_id=b03cc98e2c7da3ee&cms_redirect=yes&cmsv=e&ipbypass=yes&mh=4D&mip=27.116.48.74&mm=31&mn=sn-bu2a-nu86&ms=au&mt=1721020229&mv=u&mvi=1&pcm2cms=yes&pl=24&lsparams=ipbypass,mh,mip,mm,mn,ms,mv,mvi,pcm2cms,pl&lsig=AHlkHjAwRQIhANVQo3ftJm71nEuLZpDvy03Fdwpcc2jz8ITmci2Ex6DHAiB5trT48C90s5Uuo7pnHNPzQCERLKJ2F9yj2Kn4SimG6w%3D%3D',
+        audio: './Audio/Pushpa Pushpa - Pushpa 2.mp3',
         icon: `<i class="fa-solid fa-play"></i>`
     },
     {
         image: "https://c.saavncdn.com/178/Savarne-Lage-Hindi-2024-20240508122731-500x500.jpg",
         name: "Savarne Lage",
-        audio: 'https://rr4---sn-cvh7knze.googlevideo.com/videoplayback?expire=1721042997&ei=1bOUZtWBDIGkjuMPo-KE4Ag&ip=14.139.189.168&id=o-APO7qNmqNopalVONwfuq1r5TqFwFJXHOjNFlvtCcmt3u&itag=251&source=youtube&requiressl=yes&xpc=EgVo2aDSNQ%3D%3D&gcr=in&vprv=1&svpuc=1&mime=audio%2Fwebm&rqh=1&gir=yes&clen=2957153&dur=181.241&lmt=1714656335428912&keepalive=yes&c=ANDROID_TESTSUITE&txp=4402434&sparams=expire%2Cei%2Cip%2Cid%2Citag%2Csource%2Crequiressl%2Cxpc%2Cgcr%2Cvprv%2Csvpuc%2Cmime%2Crqh%2Cgir%2Cclen%2Cdur%2Clmt&sig=AJfQdSswRAIgI0Qe8h7YApYEfYH_l1PHOqSUVTzsnrxb9ooL5blwE-YCIG-hDKrapy4KoCr8d_mXRV7wSxh4jq6unutKQm2m2P-9&rm=sn-o3o-itqz7e,sn-h55ez76&fexp=24350516,24350518&req_id=38adbc03a22a3ee&redirect_counter=2&rrc=104&cms_redirect=yes&cmsv=e&ipbypass=yes&mh=bc&mip=27.116.48.74&mm=29&mn=sn-cvh7knze&ms=rdu&mt=1721021096&mv=m&mvi=4&pl=24&lsparams=ipbypass,mh,mip,mm,mn,ms,mv,mvi,pl&lsig=AHlkHjAwRQIgU2u4L7s4OTka1zhqRyCM58eepmXjGKXaEmkqIfx7zikCIQCIKf_i7073oczN1lvVk0rtNCBlzMbOplNOWwaM9wVgIg%3D%3D',
+        audio: './Audio/Savarne Lage - Laqshay Kapoor.mp3',
         icon: `<i class="fa-solid fa-play"></i>`
     },
    
@@ -59,10 +59,10 @@ let musics = [
 const handleData = (e) => {
     document.getElementById("imageBig").src = e.image
     // console.log(e.audio);
-    let aud = document.getElementById("boxTwoThree")
+    let aud = document.getElementById("audioTag")
     aud.src = e.audio
 
-    e.icon.classList.add("fa", "fa-pause", "play-icon")
+    // e.icon.classList.add("fa", "fa-pause", "play-icon")
 }
 
 const Mapper = (audios) => {
