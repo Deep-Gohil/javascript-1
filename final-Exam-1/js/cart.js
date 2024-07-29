4adb92f991d41885e3275ac44c30c62ca5c93bd3
Deep-Gohil <deep72617626@gmail.com>
2024-07-29 16:12:07 +0530

    Final-Exam-1

--- final-Exam-1/js/cart.js
+++ final-Exam-1/js/cart.js
@@ -1,3 +1,48 @@
 import { navbar } from "../components/navbar.js";
-document.getElementById("navbar").innerHTML = navbar()
+document.getElementById("navbar").innerHTML = navbar();
 
+const loadCart = () => {
+    let cart = JSON.parse(localStorage.getItem("cart")) || [];
+    let cartItems = document.getElementById("cart-items");
+    cartItems.innerHTML = "";
+    
+    cart.forEach(item => {
+        let div = document.createElement("div");
+        div.classList.add("cart-item");
+
+        let image = document.createElement("img");
+        image.src = item.image;
+        image.classList.add("img-fluid", "images");
+
+        let name = document.createElement("h2");
+        name.innerHTML = item.name;
+
+        let quantity = document.createElement("p");
+        quantity.innerHTML = `Quantity: ${item.quantity}`;
+
+        let increase = document.createElement("button");
+        increase.innerHTML = "+";
+        increase.addEventListener("click", () => {
+            item.quantity += 1;
+            localStorage.setItem("cart", JSON.stringify(cart));
+            loadCart();
+        });
+
+        let decrease = document.createElement("button");
+        decrease.innerHTML = "-";
+        decrease.addEventListener("click", () => {
+            if (item.quantity > 1) {
+                item.quantity -= 1;
+            } else {
+                cart = cart.filter(cartItem => cartItem.id !== item.id);
+            }
+            localStorage.setItem("cart", JSON.stringify(cart));
+            loadCart();
+        });
+
+        div.append(image,name, quantity,decrease, increase);
+        cartItems.append(div);
+    });
+};
+
+loadCart();
--- final-Exam-1/js/script.js
+++ final-Exam-1/js/script.js
@@ -1,100 +1,114 @@
 import { navbar } from "../components/navbar.js";
-document.getElementById("navbar").innerHTML = navbar()
+document.getElementById("navbar").innerHTML = navbar();
 
 const API = async () => {
-    let request = await fetch("https://json-server-deployment-7yxg.onrender.com/recepies")
-    let response = await request.json()
+    let request = await fetch("https://json-server-deployment-7yxg.onrender.com/recepies");
+    let response = await request.json();
     return response;
-}
+};
 
 const Caller = async (mealType = null) => {
-    let response = await API()
+    let response = await API();
     if (response != null) {
         if (mealType) {
             response = response.filter(ele => ele.mealType.includes(mealType));
         }
-        Mapper(response)
-        let loader = document.getElementById("loader")
-        loader.classList.add('d-none');
-        let main = document.querySelector(".loadter-main")
-        main.classList.add("d-none")
+        Mapper(response);
+        document.getElementById("loader").classList.add('d-none');
+        document.querySelector(".loadter-main").classList.add("d-none");
     } else {
-        let loader = document.getElementById("loader")
-        loader.classList.remove('d-block');
-        let main = document.querySelector(".loadter-main")
-        main.classList.add("d-block")
+        document.getElementById("loader").classList.remove('d-block');
+        document.querySelector(".loadter-main").classList.add("d-block");
     }
-}
+};
 
-Caller()
+Caller();
 
 const Mapper = (data) => {
     document.getElementById("recepies").innerHTML = "";
     data.map((ele) => {
-        let div = document.createElement("div")
-        div.classList.add("div")
+        let div = document.createElement("div");
+        div.classList.add("div");
 
-        let image = document.createElement("img")
-        image.src = ele.image
-        image.classList.add("img-fluid", "images")
+        let image = document.createElement("img");
+        image.src = ele.image;
+        image.classList.add("img-fluid", "images");
 
-        let name = document.createElement("h1")
+        let name = document.createElement("h1");
         name.innerHTML = ele.name;
-        name.classList.add("title")
+        name.classList.add("title");
 
-        let rating = document.createElement("h6")
+        let rating = document.createElement("h6");
         rating.innerHTML = "Rating: " + ele.rating;
 
-        let mealType = document.createElement("h5")
+        let mealType = document.createElement("h5");
         mealType.innerHTML = "Meal Type: " + ele.mealType.join(", ");
 
-        let country = document.createElement("h6")
+        let country = document.createElement("h6");
         country.innerHTML = "Country: " + ele.cuisine;
 
-        let recepi = document.createElement("span")
-        recepi.classList.add("recepi")
+        let recepi = document.createElement("span");
+        recepi.classList.add("recepi");
 
-        let viewRecepie = document.createElement("button")
-        viewRecepie.innerHTML = "View Recepie"
-        viewRecepie.classList.add("btn", "btn-primary")
+        let viewRecepie = document.createElement("button");
+        viewRecepie.innerHTML = "View Recepie";
+        viewRecepie.classList.add("btn", "btn-primary");
         viewRecepie.addEventListener("click", () => {
-            window.location.href = "/final-Exam-1/pages/recepie.html"
-            localStorage.setItem("recepie", JSON.stringify(ele))
-        })
+            window.location.href = "/final-Exam-1/pages/recepie.html";
+            localStorage.setItem("recepie", JSON.stringify(ele));
+        });
 
-        let Order = document.createElement("button")
-        Order.innerHTML = "Order Now"
-        Order.classList.add("btn", "btn-primary")
+        let Order = document.createElement("button");
+        Order.innerHTML = "Order Now";
+        Order.classList.add("btn", "btn-primary");
         Order.addEventListener("click", () => {
-            localStorage.setItem("order", JSON.stringify(ele))
-            window.location.href = "/final-Exam-1/pages/cart.html"
-        })
+            addToCart(ele);
+            // window.location.href = "/final-Exam-1/pages/cart.html";
+            alert("Added To Cart")
+        });
 
-        recepi.append(viewRecepie, Order)
+        recepi.append(viewRecepie, Order);
 
-        div.append(image, name, rating, mealType, country, recepi)
+        div.append(image, name, rating, mealType, country, recepi);
 
-        document.getElementById("recepies").append(div)
-    })
-}
+        document.getElementById("recepies").append(div);
+    });
+};
+
+const addToCart = (item) => {
+    let cart = JSON.parse(localStorage.getItem("cart")) || [];
+    let existingItem = cart.find(cartItem => cartItem.id === item.id);
+    if (existingItem) {
+        existingItem.quantity += 1;
+    } else {
+        item.quantity = 1;
+        cart.push(item);
+    }
+    localStorage.setItem("cart", JSON.stringify(cart));
+};
 
 const handleDinner = () => {
     Caller("Dinner");
-}
+};
 
 const handleLunch = () => {
     Caller("Lunch");
-}
+};
 
 const handleSnack = () => {
     Caller("Snack");
-}
+};
 
 const handleAll = () => {
-    Caller(); 
-}
+    Caller();
+};
 
-document.getElementById("dinner").addEventListener("click", handleDinner)
-document.getElementById("lunch").addEventListener("click", handleLunch)
-document.getElementById("snack").addEventListener("click", handleSnack)
-document.getElementById("all").addEventListener("click", handleAll)
+document.getElementById("dinner").addEventListener("click", handleDinner);
+document.getElementById("lunch").addEventListener("click", handleLunch);
+document.getElementById("snack").addEventListener("click", handleSnack);
+document.getElementById("all").addEventListener("click", handleAll);
+
+document.addEventListener("contextmenu", (e)=>{
+    e.preventDefault();
+    alert("Right click disabled");
+})
